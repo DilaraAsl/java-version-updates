021e39de3ff741b8a7d31a1a35f957437557bb06
DilaraAsl <dcavus@gmail.com>
2022-08-20 08:30:40 -0700

    new line added

--- m6_stream/src/main/java/com/cydeo/task/BranchTest.java
+++ m6_stream/src/main/java/com/cydeo/task/BranchTest.java
@@ -3,5 +3,7 @@ package com.cydeo.task;
 public class BranchTest {
     public static void main(String[] args) {
         System.out.println("Added a branch");
+
+        System.out.println(" ******** ");
     }
 }
